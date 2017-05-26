9f3e031ef9db9627b025366052b598cccb7d3aa4
ksandren <ksandren2@gmail.com>
2017-05-26 13:06:44 +0700

    one step undo. logs redirection

---
+++ undo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+    echo "Этот скрипт нужно запускать с правами суперпользователя!"
+    echo "Попробуй sudo $0"  
+    exit 1
+fi
+declare -a oldUpdates=`ls installed-updates -r`
+for update in ${oldUpdates[@]}
+do
+    echo "откат $update"
+    "./installed-updates/$update/undo.sh"
+    rm -rf "installed-updates/$update"
+    break
+done
